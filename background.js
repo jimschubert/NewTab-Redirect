ae814443e205e47c49ea31fe1a4c515188c21218
Jim Schubert <james.schubert@gmail.com>
2013-10-22 22:45:29 -0700

    Modifications to sync/welcome page
    This is related to #15 and possibly #12.
    
    This commit cleans up how local vs. sync options are figured out on startup.
    
    Now, when the install event fires for chrome_update nothing will happen. Install and Update logic has changed, so this needs to be beta tested for approval.
    
    Also, included download font for forcing a sync of the URL (chrome.storage.sync won't immediately sync if you're offline and go online). See http://fontawesome.io for font details.

--- background.js
+++ background.js
@@ -1,51 +1,124 @@
+var allOptions = ["usingStorageApi", "url", "syncOptions", "lastInstall", "showWelcome"];
 function init() {
-    // Need to do some cleanup to convert to chrome.storage
-    chrome.storage.local.get([ "usingStorageApi", "url" ], function(items) {
-        var options = items;
-        if(!options["usingStorageApi"]){
-            var arr = window.localStorage.options;
-            if (arr) {
-                options = JSON.parse(arr);
-            }
-            options["usingStorageApi"] = true;
-        }
+    console.log("background.js: init()");
+}
 
-        if (!options.url) {
-            options.url = chrome.extension.getURL("options.html");
-        }
-        chrome.storage.local.set(options);
-    });
+function saveInitial() {
+    console.log("background.js: Initial setup.");
+    var options = {};
+    var arr = window.localStorage.options;
+    if (arr) {
+        options = JSON.parse(arr);
+    }
+
+    // by default, initial installs won't sync options
+    options["syncOptions"] = false;
+    options["usingStorageApi"] = true;
+    options["showWelcome"] = true;
+
+    if (!options.url) {
+        options.url = chrome.extension.getURL("options.html");
+    }
+
+    options["lastInstall"] = +new Date();
+
+    console.log("trying to save these options", options);
+    save(options, "local");
 }
 
 // When installed, show welcome page
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.storage.local.get("showWelcome", function(items) {
-        if( (''+items["showWelcome"]) != "false") {
-            chrome.tabs.create({"url": "welcome.html" });
-        } else {
-            console.log("background.js: options have suppressed welcome page");
+chrome.runtime.onInstalled.addListener(function (details) {
+
+    var current = +new Date();
+    var sixMonths = 15552000;
+
+    if (details.reason === "chrome_update") {
+        return;
+    } else if (details.reason === "install" || details.reason === "update") {
+        return retrieve(allOptions, "local", function (localQuery) {
+            retrieve(allOptions, "sync", function (query) {
+                console.log("Pulled sync options:", query);
+
+                // canShowWelcome is always the latest sync date.
+                var canShowWelcome = (query.lastInstall > 1) && ((current - query.lastInstall) > sixMonths);
+                if (localQuery.showWelcome == false || query.showWelcome == false) {
+                    canShowWelcome = false;
+                }
+
+                var options = {};
+
+                // user previously installed on another machine, either sync or do initial setup
+                if (query["syncOptions"]) {
+                    console.log("saving sync option setup");
+                    allOptions.forEach(function (elem) {
+                        options[elem] = query[elem];
+                    });
+
+                    options["lastInstall"] = current;
+                    save(options, "local");
+                } else if(details.reason === "install") {
+                    // User hasn't previously installed, save defaults
+                    console.log("saving initial setup (not syncing)");
+                    saveInitial();
+                }
+
+                // be sure to save when we last installed (or updated)
+                save({ "lastInstall": +new Date() }, "sync");
+
+                // on initial install, or every 6 months, show Welcome Page
+                if (details.reason === "install" || canShowWelcome) {
+                    console.log("background.js: showing welcome page");
+                    return chrome.tabs.create({"url": "welcome.html" });
+                }
+            });
+        });
+    }
+});
+
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+    retrieve("syncOptions", "local", function (items) {
+        if (items.syncOptions == "false" || namespace != "sync") return;
+
+        var saveObj = {};
+        for (var key in changes) {
+            if (changes.hasOwnProperty(key)) {
+                var change = changes[key];
+                console.log('background.js: "%s|%s" changed. "%s" -> "%s"',
+                    namespace,
+                    key,
+                    change.oldValue,
+                    change.newValue);
+
+                saveObj[key] = change.newValue;
+            }
+        }
+        if(Object.keys(saveObj).length > 0) {
+            console.log("Saving sync values locally");
+            save(saveObj, "local");
         }
     });
 });
 
-chrome.storage.onChanged.addListener(function(changes, namespace) {
-    chrome.storage.local.get("syncOptions", function(items) {
-        for (key in changes) {
-            var change = changes[key];
-            console.log('background.js: "%s|%s" changed. "%s" -> "%s"',
-                namespace,
-                key,
-                change.oldValue,
-                change.newValue);
-            
-            if(items.syncOptions && namespace === "sync") {
-                console.log('background.js: updating synced value %s=%s', key, change.newValue);
-                var save = {};
-                save[key] = change.newValue;
-                chrome.storage.local.set(save);
-            }
+function save(items, area) {
+    chrome.storage.local.get(["syncOptions"], function (localQuery) {
+        if (localQuery.syncOptions == false) {
+            // if user doesn't want to save, we'll always sync to local
+            area = "local";
         }
+
+        console.log("Saving the following items to " + area + ":", items);
+        chrome.storage[area].set(items);
     });
-});
+}
+
+function retrieve(items, area, cb) {
+    if ("function" !== typeof cb) {
+        cb = function (items) {
+            console.log("items:", items);
+        }
+    }
+
+    chrome.storage[area].get(items, cb);
+}
 
 init();
