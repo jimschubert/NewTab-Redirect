282a948a24969b08187d6cae57407e36e7ffb1d9
Jim Schubert <james.schubert@gmail.com>
2013-10-24 20:27:53 -0700

    Remove console.log calls in redirect.js

--- redirect.js
+++ redirect.js
@@ -7,8 +7,6 @@ function r(tabId, url) {
 
 function init(){    
     chrome.storage.local.get("url", function(items) {
-	console.log("Items:");
-	console.log(items);
 	var url = items.url || "";
 	if ((/^http:/i.test(url)) || /^https:/i.test(url)) {
 	    document.location.href = url;
