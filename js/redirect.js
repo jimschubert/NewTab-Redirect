0459ec2224101ef49a6e6a522a6143176fc1675e
Jamie Sharpe <github@jamiesharpe.co.uk>
2014-04-25 15:36:31 +0100

    Fixed property change.
    Fixed a property change for users that may have overridden it.

--- js/redirect.js
+++ js/redirect.js
@@ -1,15 +1,15 @@
 /*global chrome,document,window */
 (function init() {
-    "use strict";
-    chrome.storage.local.get(["url","tab.highlighted"], function (items) {
+    "use strict";    
+    chrome.storage.local.get(["url","tab.selected"], function (items) {
         var url = items.url;
         if(url) {
             // a keen user may open the extension's background page and set:
             // chrome.storage.local.set({'tab.selected': false});
-            var highlighted = items["tab.highlighted"] === undefined ? true : (items["tab.highlighted"] == "true");
+            var highlighted = items["tab.selected"] === undefined ? true : (items["tab.selected"] == "true");
             chrome.tabs.update({
                 "url": url,
-                "highlighted": highlighted,
+                "highlighted": highlighted
             });
         } else {
             angular.resumeBootstrap();
