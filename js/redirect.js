31c4263b03676c82ad3bd03b9146a55517ef50c2
Jim Schubert <james.schubert@gmail.com>
2014-08-06 12:52:56 -0400

    Only get current tab if using chrome.tabs.update to prevent slowing down regular redirects

--- js/redirect.js
+++ js/redirect.js
@@ -1,25 +1,25 @@
 /*global chrome,document,window */
 (function init() {
     "use strict";
-    chrome.tabs.getCurrent(function(tab){
-        chrome.storage.local.get(["url","tab.selected", "always-tab-update"], function (items) {
-            var url = items.url;
-            if(url) {
-                url = (0 !== url.indexOf("about:") && -1 === url.indexOf("://")) ? ("http://"+url) : url;
-                if (/^http[s]?:/i.test(url) && items["always-tab-update"] !== true) {
-                    document.location.href = url;
-                } else {
+    chrome.storage.local.get(["url", "tab.selected", "always-tab-update"], function (items) {
+        var url = items.url;
+        if (url) {
+            url = (0 !== url.indexOf("about:") && -1 === url.indexOf("://")) ? ("http://" + url) : url;
+            if (/^http[s]?:/i.test(url) && items["always-tab-update"] !== true) {
+                document.location.href = url;
+            } else {
+                chrome.tabs.getCurrent(function (tab) {
                     // a keen user may open the extension's background page and set:
                     // chrome.storage.local.set({'tab.selected': false});
                     var selected = items["tab.selected"] === undefined ? true : (items["tab.selected"] == "true");
                     chrome.tabs.update(tab.id, {
-                        "url"     : url,
+                        "url": url,
                         "highlighted": selected
                     });
-                }
-            } else {
-                angular.resumeBootstrap();
+                });
             }
-        });
+        } else {
+            angular.resumeBootstrap();
+        }
     });
 }());
