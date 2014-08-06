7fd639a0c39686ce39c91b82623489da15d1fd86
Jim Schubert <james.schubert@gmail.com>
2014-08-06 12:49:38 -0400

    Explicitly provide tabId on chrome.tabs.update

--- js/redirect.js
+++ js/redirect.js
@@ -1,23 +1,25 @@
 /*global chrome,document,window */
 (function init() {
     "use strict";
-    chrome.storage.local.get(["url","tab.selected", "always-tab-update"], function (items) {
-        var url = items.url;
-        if(url) {
-            url = (0 !== url.indexOf("about:") && -1 === url.indexOf("://")) ? ("http://"+url) : url;
-            if (/^http[s]?:/i.test(url) && items["always-tab-update"] !== true) {
-                document.location.href = url;
+    chrome.tabs.getCurrent(function(tab){
+        chrome.storage.local.get(["url","tab.selected", "always-tab-update"], function (items) {
+            var url = items.url;
+            if(url) {
+                url = (0 !== url.indexOf("about:") && -1 === url.indexOf("://")) ? ("http://"+url) : url;
+                if (/^http[s]?:/i.test(url) && items["always-tab-update"] !== true) {
+                    document.location.href = url;
+                } else {
+                    // a keen user may open the extension's background page and set:
+                    // chrome.storage.local.set({'tab.selected': false});
+                    var selected = items["tab.selected"] === undefined ? true : (items["tab.selected"] == "true");
+                    chrome.tabs.update(tab.id, {
+                        "url"     : url,
+                        "highlighted": selected
+                    });
+                }
             } else {
-                // a keen user may open the extension's background page and set:
-                // chrome.storage.local.set({'tab.selected': false});
-                var selected = items["tab.selected"] === undefined ? true : (items["tab.selected"] == "true");
-                chrome.tabs.update({
-                    "url"     : url,
-                    "highlighted": selected
-                });
+                angular.resumeBootstrap();
             }
-        } else {
-            angular.resumeBootstrap();
-        }
+        });
     });
 }());
