39e0f700e1f9797989e11455130b4857073d214a
Dan Kaplun <dbkaplun@gmail.com>
2016-06-26 13:30:57 -0700

    Implement <iframe> redirect mechanism
    Add "Redirect mechanism" <select> that replaces "always update tab" checkbox
    Deprecate 'always-tab-update' option, replace with 'redirectMechanism'
    
    Closes #122

--- js/redirect.js
+++ js/redirect.js
@@ -2,22 +2,31 @@
 (function init(angular) {
     "use strict";
     try {
-        chrome.storage.local.get(["url", "tab.selected", "always-tab-update"], function (items) {
+        chrome.storage.local.get(["url", "tab.selected", "redirectMechanism", /*deprecated*/"always-tab-update"], function (items) {
             var url = items.url;
             if (url) {
                 url = (0 !== url.indexOf("about:") && 0 !== url.indexOf("data:") && -1 === url.indexOf("://")) ? ("http://" + url) : url;
-                if (/^http[s]?:/i.test(url) && items["always-tab-update"] !== true) {
-                    document.location.href = url;
-                } else {
-                    chrome.tabs.getCurrent(function (tab) {
-                        // a keen user may open the extension's background page and set:
-                        // chrome.storage.local.set({'tab.selected': false});
-                        var selected = items["tab.selected"] === undefined ? true : (items["tab.selected"] == "true");
-                        chrome.tabs.update(tab.id, {
-                            "url": url,
-                            "highlighted": selected
+                switch (items.redirectMechanism || (items["always-tab-update"] ? 'update' : 'redirect')) {
+                    case 'redirect':
+                        document.location.href = url;
+                        break;
+                    case 'update':
+                        chrome.tabs.getCurrent(function (tab) {
+                            // a keen user may open the extension's background page and set:
+                            // chrome.storage.local.set({'tab.selected': false});
+                            var selected = items["tab.selected"] === undefined ? true : (items["tab.selected"] == "true");
+                            chrome.tabs.update(tab.id, {
+                                "url": url,
+                                "highlighted": selected
+                            });
                         });
-                    });
+                        break;
+                    case 'iframe':
+                        var iframe = document.createElement('iframe');
+                        iframe.src = url;
+                        document.body.style.overflow = 'hidden';
+                        document.body.appendChild(iframe);
+                        break;
                 }
             } else {
                 angular.resumeBootstrap();
